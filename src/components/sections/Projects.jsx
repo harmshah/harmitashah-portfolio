48c5e355d44fd2e0091cfaaa1c5742c938cb6560
Harmita Shah <harmitashah07@gmail.com>
2025-05-14 09:33:01 -0400

    forth commit

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -16,10 +16,24 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
               <h3 className="text-xl font-bold mb-2"> Empower Her Soft</h3>
               <p className="text-gray-400 mb-4">
-                Project Description here...
+                Empower Her Soft is a women-led digital platform created to
+                support women navigating life transitions—whether re-entering
+                the workforce, rebuilding after personal challenges, or
+                launching entirely new paths. Unlike traditional job boards or
+                coaching hubs, Empower Her Soft is a soft landing and a
+                launchpad, combining emotional empowerment, financial
+                confidence, and community-rooted growth.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["React", "Node.js", "Javascript", "MongoDB"].map((tech, key) => (
+                {[
+                  "React.js",
+                  "Node.js + Express.js",
+                  "JavaScript (ES6+)",
+                  "CSS / Bootstrap ",
+                  "MongoDB",
+                  "Render",
+                  "Vercel",
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -34,13 +48,14 @@ export const Projects = () => {
               <div className="flex justify-between items-center">
                 <a
                   href="https://empower-her-soft.vercel.app"
-                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-500 hover:text-blue-400 transition-colors my-4 text-lg font-medium"
                 >
                   View Project →
                 </a>
               </div>
             </div>
-            
           </div>
         </div>
       </RevealOnScroll>
